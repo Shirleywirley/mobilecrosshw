85e2916629b2d05dc08369a3a9a98c29682aa801
ShirleyWhirley <99760675+Shirleywirley@users.noreply.github.com>
2022-04-03 22:19:21 -0700

    Update idk.js

--- idk.js
+++ idk.js
@@ -1,10 +1,10 @@
 var shirley = prompt("What is your name?");
-document.getElementById("shirley").innerHTML = me;
+document.getElementById("shirley").innerHTML = shirley;
 
 var huh = prompt(
   "Would you kindly guess my favorite kpop group? Oh- you don't know? Too bad- hurry up and guess!"
 );
-document.getElementById("exo").innerHTML = you;
+document.getElementById("exo").innerHTML = huh;
 
 var stop = prompt(
   "Now could you kindly tell me your favorite singer? Hey- after you struggled to guess, I'm being nice."
@@ -14,10 +14,10 @@ document.getElementById("april").innerHTML = stop + ":()";
 var nawr = prompt(
   "Alright, I have no idea what I just did, but type whatever. Just do it."
   );
-document.getElementById("fools").innerHTML = her;
+document.getElementById("fools").innerHTML = nawr;
 
 var ohorat = prompt("So~ would you kindly give me your money???");
-document.getElementById("yes").innerHTML = or;
+document.getElementById("yes").innerHTML = ohorat;
 
-var questionSix = prompt("Hey you! Who's that Pokemon?");
-document.getElementById("pikachu").innerHTML = not;
+var yawr = prompt("Hey you! Who's that Pokemon?");
+document.getElementById("pikachu").innerHTML = yawr;
