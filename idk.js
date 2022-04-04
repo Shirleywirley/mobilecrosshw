8ea696d4408b368f8c391677c2ebf0aa5d99aa71
ShirleyWhirley <99760675+Shirleywirley@users.noreply.github.com>
2022-04-03 21:34:43 -0700

    Create idk.js

---
+++ idk.js
@@ -0,0 +1,23 @@
+var shirley = prompt("What is your name?");
+document.getElementById("shirley").innerHTML = me;
+
+var huh = prompt(
+  "Would you kindly guess my favorite kpop group? Oh- you don't know? Too bad- hurry up and guess!"
+);
+document.getElementById("exo").innerHTML = you;
+
+var stop = prompt(
+  "Now could you kindly tell me your favorite singer? Hey- after you struggled to guess, I'm being nice."
+);
+document.getElementById("idk").innerHTML = stop + ":()";
+
+var nawr = prompt(
+  "Alright, I have no idea what I just did, but type whatever. Just do it."
+  );
+document.getElementById("um what").innerHTML = her;
+
+var ohorat = prompt("So~ would you kindly give me your money???");
+document.getElementById("yes").innerHTML = or;
+
+var questionSix = prompt("Hey you! Who's that Pokemon?");
+document.getElementById("pikachu").innerHTML = not;
