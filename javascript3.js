1aa94362b5daf7a0d470e8daabac5e47a4f71066
ShirleyWhirley <99760675+Shirleywirley@users.noreply.github.com>
2022-05-15 21:28:20 -0700

    Update javascript3.js

--- javascript3.js
+++ javascript3.js
@@ -1,12 +1,12 @@
 let canvas;
 let ctx;
 
-let incrementor = 600;
+let incrementor = 450;
 let decrementor = 500;
-let whee = incrementor + 100;
-let what = incrementor + 50;
-let why = decrementor + 600;
-let who = decrementor / 5;
+let you = incrementor + 800;
+let are = incrementor + 250;
+let a = decrementor + 365;
+let nugget = decrementor / 90;
 
 window,onload = function () {
   canvas = document.getElementById("creativeCoding");
@@ -18,15 +18,15 @@ window,onload = function () {
 
 
 function drawBoxxy() {
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "white";
   ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-  ctx.fillStyle = "blue";
-  ctx.fillRect(decrementor--, 10, 100, 100);
+  ctx.fillStyle = "yellow";
+  ctx.fillRect(decrementor--, 10, 85, 30);
   ctx.fillStyle = "green";
-  ctx.fillRect(decrementor--, why--, 250, 250);
+  ctx.fillRect(decrementor--, a--, 450, 350);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(400, decrementor--, incrementor++, are--);
   ctx.fillStyle = "red";
-  ctx.fillRect(400, decrementor--, incrementor++, what--);
-  ctx.fillStyle = "magenta";
-  ctx.fillRect(who++, decrementor++, 300, 250);
+  ctx.fillRect(nugget++, decrementor++, 150, 250);
 }
