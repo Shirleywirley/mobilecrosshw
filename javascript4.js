c007866eee73713847bba4cd7c8f19cd8ecb4a12
ShirleyWhirley <99760675+Shirleywirley@users.noreply.github.com>
2022-05-25 14:07:46 -0700

    Update javascript4.js

--- javascript4.js
+++ javascript4.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("creativeCoding");
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 var ctx = canvas.getContext("2d");
 
 var x = 0;
@@ -8,27 +8,28 @@ var y = 0;
 var incrementor = 2;
 
 function Square() {
-  ctx.fillStyle = "#DAF7A6";
-  ctx.fillRect(x, y, 300, 500);
+  ctx.fillStyle = "#FFC300";
+  ctx.fillRect(x, y, 200, 100);
 
-  ctx.fillStyle = "#BB8FCE ";
+  ctx.fillStyle = "#FF5733";
   ctx.fillRect(x, 1, 10, 100);
 
-  ctx.fillStyle = "#CE9B8F";
-  ctx.fillRect(10, y, 100, 100);
+  ctx.fillStyle = "#C70039 ";
+  ctx.fillRect(20, y, 360, 50);
 }
 
 function drawBoxxy() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 
-  ctx.font = "50px exo";
-  ctx.fillStyle = "red";
-  ctx.fillText("ohorat", x, 250);
-  ctx.fillText("wee", 500, y);
 
+  ctx.font = "25px Exo";
+  ctx.fillStyle = "#C2CE8F";
+  ctx.fillText("no", x, 250);
+  ctx.fillText("Yehorat", 500, y);
 
-  let something = "yehet";
+ 
+  let something = "ohorat";
   ctx.fillText(something, x, y);
 
   Square();
