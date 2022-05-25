8d4e9089cf25c26ccf482f1c44a9187786f6848c
ShirleyWhirley <99760675+Shirleywirley@users.noreply.github.com>
2022-05-25 13:32:39 -0700

    Update javascript4.js

--- javascript4.js
+++ javascript4.js
@@ -4,27 +4,42 @@ canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
 
 var x = 0;
-
-var incrementor = 50;
-var decrementor = 100
+var y = 0;
+var incrementor = 2;
 
 function Square() {
-  ctx.fillStyle = "red";
-  ctx.fillRect(x, 25, 24, 23);
+  ctx.fillStyle = "#DAF7A6";
+  ctx.fillRect(x, y, 300, 500);
+
+  ctx.fillStyle = "#BB8FCE ";
+  ctx.fillRect(x, 1, 10, 100);
+
+  ctx.fillStyle = "#CE9B8F";
+  ctx.fillRect(10, y, 100, 100);
 }
 
 function drawBoxxy() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+  ctx.font = "50px exo";
+  ctx.fillStyle = "red";
+  ctx.fillText("ohorat", x, 250);
+  ctx.fillText("wee", 500, y);
+
+
+  let something = "yehet";
+  ctx.fillText(, x, y);
+
   Square();
-  
-   while (x + incrementor > canvas.width || x + incrementor < 0) {
+  while (x + incrementor > canvas.width || x + incrementor < 0) {
     incrementor = -incrementor;
   }
-
+  while (y + incrementor > canvas.height || y + incrementor < 0) {
+    incrementor = -incrementor;
+  }
   x += incrementor;
-  console.log(incrementor + "  incrementor value");
-
-
+  y += incrementor;
 }
 
 setInterval(drawBoxxy, 10);
